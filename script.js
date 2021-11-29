2aadb61ed35149f138d50abdf04b7abcf179d478
Angélica Silva <51302207+AngelicaCSSilva@users.noreply.github.com>
2021-11-29 17:00:05 -0300

    Adicionada descrição breve das funções.

--- script.js
+++ script.js
@@ -3,15 +3,18 @@ const textMeme = document.querySelector('#meme-text');
 const file = document.querySelector('#meme-insert');
 const memeContainer = document.querySelector('#meme-image-container');
 
+// Função que adiciona o valor do input dentro do elemento p#meme-text
 // ref.: https://stackoverflow.com/questions/14411235/while-typing-in-a-text-input-field-printing-the-content-typed-in-a-div
 function insertTextOnDiv() {
   textMeme.innerText = textInput.value;
 }
 
+// Função que adiciona o Event Listener de realizar o insert do texto enquanto estiver sendo digitado no campo de input.
 function showTextWhileTyping() {
   textInput.addEventListener('keyup', insertTextOnDiv);
 }
 
+// Função que adiciona a imagem dentro do do div#meme-image-container
 // ref.: https://stackoverflow.com/questions/11708797/inserting-a-file-input-as-an-img-in-the-dom
 function addImage() {
   const img = document.createElement('img');
@@ -20,6 +23,7 @@ function addImage() {
   memeContainer.appendChild(img);
 }
 
+// Função que adiciona o Event Listener no input(file) para realizar a adição da imagem ao container
 function btnImg() {
   file.addEventListener('change', addImage);
 }
